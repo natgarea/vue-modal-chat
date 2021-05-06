82d711e5688e4388a4235b8a613c63c41c2b73f0
Natalia Garea <natgarea@gmail.com>
2021-05-06 20:14:09 +0200

    Fix attachments urls

--- src/components/Chat/Body/Message.vue
+++ src/components/Chat/Body/Message.vue
@@ -31,7 +31,7 @@
               <div class="d-flex flex-row justify-content-between">
                 <div class="d-inline-block">
                   <p class="mb-0 text-uppercase font-weight-bold">Versión {{message.version}}</p>
-                  <p class="mb-0"><a href="message.url">{{message.title}}</a></p>
+                  <p class="mb-0"><a :href="message.URL">{{message.title}}</a></p>
                   <p class="mb-0 mt-1 small">
                     <span class="mr-3">Documento / {{message.file_type}} ({{message.file_size}})</span>
                     <span>{{moment(message.timestamp).format('DD MMM YYYY / kk:mm A')}}</span>
